03c515f8ab9f2379d3b678896a997300b2071570
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-04-10 17:48:46 -0300

    first commit

---
+++ main.lua
@@ -0,0 +1,31 @@
+function love.load()
+    wf = require 'libraries/windfield'
+    world = wf.newWorld(0, 500)
+
+    player = world:newRectangleCollider(360, 100, 80, 80)
+    player.speed = 240
+    platform = world:newRectangleCollider(250, 400, 300, 100)
+    platform:setType("static")
+end
+
+function love.update(dt)
+    world:update(dt)
+
+    local px, py = player:getPosition()
+    if love.keyboard.isDown('right') then
+        player:setX(px + player.speed * dt)
+    end
+    if love.keyboard.isDown('left') then
+        player:setX(px - player.speed * dt)
+    end
+end
+
+function love.draw()
+    world:draw()
+end
+
+function love.keypressed(key)
+    if key == 'up' then
+        player:applyLinearImpulse(0, -5000)
+    end
+end
